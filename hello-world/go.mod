bfd710bb3168a99b1f42f5b58d714f43a47580c3
Revanth Badveli <revanth.badveli@traceable.ai>
2025-05-11 13:31:42 +0530

    add dep

--- hello-world/go.mod
+++ hello-world/go.mod
@@ -1,3 +1,5 @@
 module github.com/revanth/hello-world
 
 go 1.22
+
+require github.com/google/uuid v1.4.0
--- hello-world/main.go
+++ hello-world/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/revanth/hello-world/mathutil"
+)
 
 func main() {
 	fmt.Println("Hello, World! This is a test CI application.")
+	
+	result := mathutil.Add(5, 3)
+	fmt.Printf("5 + 3 = %d\n", result)
+	
+	id := uuid.New()
+	fmt.Printf("Generated UUID: %s\n", id.String())
 }
